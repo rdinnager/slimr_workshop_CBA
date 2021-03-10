408c88cc86779a6b1f4d2f1a1a4ecb7483e597f5
luis mijangos <luis.mijangos@unsw.edu.au>
2021-03-11 04:31:10 +1100

    Add files via upload

--- Luis_slimR/functions.R
+++ Luis_slimR/functions.R
@@ -19,7 +19,7 @@ ped <- function(df_ped){
 
 # this code returns the same LD output as the software haploview
 # this is the input file for the LD analyses
-LD_fun <- function(df_LD_fun,pop_size ,show_warnings=T){
+LD_fun <- function(df_LD_fun,pop_size ,show_warnings=F){
   df_LD_fun$V1[df_LD_fun$V1=="Male"]   <- 1
   df_LD_fun$V1[df_LD_fun$V1=="Female"] <- 2
   df_LD_fun$id <- df_LD_fun[,2]
@@ -83,7 +83,7 @@ LD_fun <- function(df_LD_fun,pop_size ,show_warnings=T){
   }
 }
 
-read.pedfile_b <- function (file, n, snps, which, split = "\t| +", sep = ".", na.strings = "0", lex.order = FALSE,show_warnings=T){
+read.pedfile_b <- function (file, n, snps, which, split = "\t| +", sep = ".", na.strings = "0", lex.order = FALSE,show_warnings=F){
   r0 <- as.raw(0)
   r1 <- as.raw(1)
   r2 <- as.raw(2)
@@ -226,102 +226,3 @@ read.pedfile_b <- function (file, n, snps, which, split = "\t| +", sep = ".", na
   }
   list(genotypes = result, fam = fam, map = map)
 }
-
-# 
-# do_LD_analysis <- function(path_folder,list_ld,list_snps,list_files){
-#   for( i in 1:nrow(list_files)){
-#     ld_columns <- read.table(paste0(getwd(),"/","ld_pop1.csv"),sep = ",",row.names = 1)
-#     # ld_columns <- read.table(paste0(path_folder,"/",list_ld[list_files[i,3]]),sep = ",",row.names = 1)
-#     colnames(ld_columns) <- rownames(ld_columns)
-#     snpsum.col <- read.table(paste0(getwd(),"/","snps_pop1.csv"),header=T,sep = ",")
-#     # if(is.na(snpsum.col)){next()}
-#     ld_columns <- as.data.frame(as.table(as.matrix(ld_columns)))
-#     ld_columns <- ld_columns[-ld_columns$Freq < 0,] #remove cases where LD was not calculated
-#     ld_columns$Var1 <- as.numeric(as.character(ld_columns$Var1))
-#     ld_columns$Var2 <- as.numeric(as.character(ld_columns$Var2))
-#     #determine the distance at which LD was calculated
-#     ld_columns$dis <- ld_columns$Var2 - ld_columns$Var1 
-#     #remove pairwise LD results that were calculated at larger distances than the required in the 
-#     # settings and then filtering and rearraging dataframes to match each other and then merge them
-#     test_linkageb <-ld_columns[which(ld_columns$dis<=ld_max_pairwise),] 
-#     ldtb <- data.table( test_linkageb , key = "Var1" )
-#     ldtc <- data.table( test_linkageb , key = "Var2" )
-#     use <- with(snpsum.col, (!is.na(MAF_pop1)))
-#     # Remove NA's
-#     use[is.na(use)] <- FALSE 
-#     # Subset genotype and SNP summary data for SNPs that pass call rate and MAF criteria
-#     snp_loc  <- snpsum.col[use,c("chr_name","row_name","loc_cM","loc_bp")] # this is the location of each snp in cM and in bp
-#     dtb <- data.table(snp_loc, key = "loc_bp" )
-#     t_locationb <- ldtb[dtb, "row_name", nomatch=0]
-#     t_locationc <- ldtc[dtb, "row_name", nomatch=0]
-#     test_linkageb <- test_linkageb[order(test_linkageb$Var1),]
-#     test_linkageb$locus_a <- t_locationb
-#     test_linkageb <- test_linkageb[order(test_linkageb$Var2),]
-#     test_linkageb$locus_b <- t_locationc
-#     test_linkageb <- test_linkageb[order(test_linkageb$Var1),]
-#     test_linkage <- cbind(test_linkageb[,5],test_linkageb[,3], test_linkageb[,4], test_linkageb[,1])
-#     colnames(test_linkage) <- c("L1","r^2","Dist","location")
-#     test_split <- split(test_linkage,test_linkage$L1) #separating snps into individual dataframes
-#     test_split <- lapply(test_split,"[",,-1)
-#     # function to calculate the mean r^2 by locus in windows of the length specified in the settings
-#     func_rsqr <- function(dfx){stats.bin(dfx$Dist, dfx$`r^2`,breaks = seq(0,ld_max_pairwise,ld_resolution))}
-#     # this function is just to obtain the same output as the above funtion so the two outputs match each 
-#     # other and therefeore possible to merge them
-#     func_loc  <- function(dfx){stats.bin(dfx$Dist, dfx$location,breaks = seq(0,ld_max_pairwise,ld_resolution))}
-#     #applying functions and merging dataframes
-#     t_rsqr <- lapply(test_split,func_rsqr)
-#     t_rsqr_b <-lapply(t_rsqr,"[[",3)
-#     t_rsqr_c <- lapply(t_rsqr_b,"[",2,)
-#     t_rsqr_d <- lapply(t_rsqr_c, as.data.frame)
-#     t_loc <- lapply(test_split,func_loc)
-#     t_loc_b <- lapply(t_loc,"[[",3)
-#     t_loc_c <- lapply(t_loc_b,"[",2,)
-#     t_loc_d <- lapply(t_loc_c, as.data.frame)
-#     linkage <- Map(cbind,t_loc_d, t_rsqr_d)
-#     linkage_b <- rbindlist(linkage)
-#     linkage_b <- as.data.frame(linkage_b)
-#     # LD has been now calculated at the same maximum distance and at the same resolution for each snp. 
-#     # A vector (based on the maximum distance of LD and its resolution) is used to separate LD 
-#     # resolutions into bins so LD can be calculated in each genomic region
-#     linkage_b$factor <- 1:(ld_max_pairwise/ld_resolution)
-#     linkage_b <- linkage_b[complete.cases(linkage_b),]
-#     colnames(linkage_b) <- c("loc","rsqr","factor")
-#     linkage_c<-linkage_b[order(linkage_b$factor),]
-#     linkage_c$factor<- factor(linkage_c$factor)
-#     linkage_c<- as.data.frame(linkage_c)
-#     # separating the groups of LD resolutions into individual dataframes
-#     linkage_split<-split(linkage_c,linkage_c$factor) 
-#     linkage_split<-lapply(linkage_split,as.data.frame)
-#     # function to calculate the mean r^2 of each genomic region in windows of length of the region_size
-#     fun_rsqr<- function(df){stats.bin(df$loc, df$rsqr,  breaks = seq(0,chromosome_length,region_size))}
-#     fun_loc<- function(df){stats.bin(df$loc, df$factor,  breaks = seq(0,chromosome_length,region_size))}
-#     link_rsqr<-lapply(linkage_split,fun_rsqr)
-#     link_rsqr_b<-lapply(link_rsqr,"[[",3)
-#     link_rsqr_c<- lapply(link_rsqr_b,"[",2,)
-#     link_rsqr_d<-lapply(link_rsqr_c, as.data.frame)
-#     link <- list.cbind(link_rsqr_d)
-#     # this is the number of non-synonymous mutations at the end of the simulations
-#     use_b <- with(snpsum.col,(RAF_pop1>0)) 
-#     # Remove NA's
-#     use_b[is.na(use_b)] <- FALSE 
-#     # Subset genotype and SNP summary data for SNPs that pass call rate and MAF criteria
-#     NS_sim <- snpsum.col[use_b,"loc_bp"] 
-#     # number_nonsyn[,i] <- as.data.frame(table(cut(NS_sim, breaks=seq(0,chromosome_length,region_size))))[,2]
-#     # this is to  calculate the means of the SNP's statistics in bins of size of the region_size 
-#     snpsum.col_2 <- snpsum.col[use_b,] 
-#     snpsum.col_2 <- split(snpsum.col_2,cut(snpsum.col_2$loc_bp,breaks=seq(0,chromosome_length,region_size)))
-#     snpsum.col_2 <- as.data.frame(snpsum.col_2[,5:ncol(snpsum.col_2)])
-#     # snpsum.col_2 <- lapply(snpsum.col_2,"[",,5:24)
-#     snpsum.col_2 <- colMeans(snpsum.col_2,,na.rm=T)
-#     snpsum.col_2 <- list.rbind(snpsum.col_2)
-#     snpsum.col_mean[[i]] <- snpsum.col_2
-#     for (ld_distance in 1: ncol(link) ) {
-#       link_iterations[[ld_distance]][,i] <- link[,ld_distance]
-#     }
-#   }
-#   return(list(link_iterations,snpsum.col_mean))
-#   # return(list(link_iterations,number_nonsyn,snpsum.col_mean))
-# }
-# 
-# 
-# 
